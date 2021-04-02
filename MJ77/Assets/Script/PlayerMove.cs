f1fec1d9f50970c7d9f2f1ff5db916b964007bbe
sammyboy47 <60956635+sammyboy47@users.noreply.github.com>
2021-04-03 02:12:53 +0800

    Movement system implemented
    -Movement has accelration
    -Mouse look is for Y-axis for now
    -Jump no longer acting weird

--- MJ77/Assets/Script/PlayerMove.cs
+++ MJ77/Assets/Script/PlayerMove.cs
@@ -11,14 +11,14 @@ public class PlayerMove : MonoBehaviour
 
     public bool isMove, isJump, isClimb;
 
-    Vector3 getInputMove { get { return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); } }
-    Vector3 inputMove, getRot;
+    Vector3 getInputMove { get { return new Vector3(Input.GetAxis("Horizontal"),0, (Input.GetAxis("Vertical"))); } }
+    [SerializeField] Vector3 inputMove, getRot, getMove;
     Quaternion targetRot;
     // Start is called before the first frame update
     // Start is called before the first frame update
     void Start()
     {
-
+        targetRot = thisRB.rotation;
     }
 
     // Update is called once per frame
@@ -37,9 +37,9 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
         {
-            getRot.x = thisRB.rotation.x + (Input.GetAxis("Mouse Y") * RotSpd);
+            // getRot.x = thisRB.rotation.x + (Input.GetAxis("Mouse Y") * -RotSpd);
             getRot.y = thisRB.rotation.y + (Input.GetAxis("Mouse X") * RotSpd);
-            targetRot = Quaternion.Euler(getRot);
+            targetRot = Quaternion.Euler(thisRB.rotation.eulerAngles + getRot);
             rotTime = 0;
         }
     }
@@ -47,18 +47,54 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         if (isMove)
-            thisRB.velocity = (inputMove * CurMove);// * Time.fixedDeltaTime;
-        if (isJump && JmpCntr < JmpTime)
         {
-            float t = JmpCntr / JmpTime, getJump = Mathf.Lerp(JmpPwr, 0, t);
-            thisRB.AddForce(new Vector3(0, getJump, 0),ForceMode.VelocityChange);
+            getMove = transform.TransformDirection(inputMove * Mathf.Lerp(0, CurMove, Accelerate * Accelerate));
+            // thisRB.velocity = (inputMove * CurMove);// * Time.fixedDeltaTime;
+            // if (thisRB.velocity.sqrMagnitude > getMove.sqrMagnitude)
+            // {
+            //     // getMove.x = Mathf.Clamp(getMove.x, -CurMove, CurMove);
+            //     // getMove.z = Mathf.Clamp(getMove.z, -CurMove, CurMove);
+            //     //getMove.y = 0;
+            //     getMove.x = (thisRB.velocity.x - getMove.x); //(getMove.x>thisRB.velocity.x) ? 0 : getMove.x - thisRB.velocity.x;
+            //     getMove.z = (thisRB.velocity.z - getMove.z);//(getMove.z > thisRB.velocity.z)?0 : getMove.z;
+            // }
+            // getMove.y = thisRB.velocity.y;
+            print($"Movement : {getMove} by {inputMove}");
+            thisRB.AddForce(getMove, ForceMode.Impulse);
+            if (thisRB.velocity.magnitude > MvSpd)
+                thisRB.velocity = new Vector3(thisRB.velocity.normalized.x * CurMove,
+                thisRB.velocity.y, thisRB.velocity.normalized.z * CurMove);
+            if (Accelerate <= 1)
+                Accelerate += Time.deltaTime;
+            else Accelerate = 1;
+        }
+        else
+        {
+            if (Accelerate > 0) Accelerate -= Time.deltaTime;
+            else if (Accelerate < 0)
+                Accelerate = 0;
+            if (thisRB.velocity.sqrMagnitude > 0)
+                thisRB.velocity = new Vector3(thisRB.velocity.x * .5f, thisRB.velocity.y, thisRB.velocity.z * .5f);
+        }
+        if (isJump && (JmpCntr < JmpTime))
+        {
+            print($"jump: {isJump}");
+            float t = JmpCntr / JmpTime;//, getJump = Mathf.Lerp(JmpPwr, 0, t);
+            // getMove.y = Mathf.Lerp(JmpPwr, 0, t);// thisRB.AddForce(new Vector3(0, getJump, 0),ForceMode.VelocityChange);
             JmpCntr += Time.deltaTime;
+            thisRB.AddForce(new Vector3(0, Mathf.Lerp(JmpPwr, 0, t), 0), ForceMode.Impulse);
         }
         else
         {
             if (JmpCntr > 0)
                 JmpCntr -= Time.deltaTime * 2;
         }
+
+
+        // thisRB.velocity = getMove;
+        // if(isMove&& (isJump&&JmpCntr<JmpTime))
+        //     thisRB.AddForce(getMove, ForceMode.VelocityChange);
+
         if (thisRB.rotation != targetRot)
         {
             thisRB.MoveRotation(targetRot);
