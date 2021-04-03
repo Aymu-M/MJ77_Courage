e6500a2710875079ce772523e045d36ceccbc487
sammyboy47 <60956635+sammyboy47@users.noreply.github.com>
2021-04-04 02:42:19 +0800

    Day2 fix
    Movement, Jump, Camera revamped and working properly

--- MJ77/Assets/Script/CamScript.cs
+++ MJ77/Assets/Script/CamScript.cs
@@ -4,34 +4,48 @@ using UnityEngine;
 
 public class CamScript : MonoBehaviour
 {
-   public Transform camRig;
-    public float rotSpd = 17;
+   public Transform camRig, locPlyr;
+    public float rotSpd = 17, pitchLimit=45;
     // Start is called before the first frame update
     void Start()
     {
-        fncGetTgtRot(transform.eulerAngles);
+        fncGetTgtRot(Vector3.zero);//transform.eulerAngles);
     }
     Vector3 camRot;
-    public Quaternion targetRot, avatarRot;
+    public Quaternion targetRot;
+    public Quaternion avatarRot(Quaternion startRot) => Quaternion.Euler(0, camRot.y, 0);
+    // Quaternion.RotateTowards(startRot, Quaternion.Euler(0, targetRot.y, 0),5);
     // Update is called once per frame
     void Update()
     {
         if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
         {
+            fncGetTgtRot((new Vector3((Input.GetAxis("Mouse Y") * -rotSpd), (Input.GetAxis("Mouse X") * rotSpd), 0)));//(new Vector3(transform.eulerAngles.x + (Input.GetAxis("Mouse Y") * -rotSpd), transform.eulerAngles.y + (Input.GetAxis("Mouse X") * rotSpd), 0)));
+        }
             // camRot = transform.eulerAngles + (new Vector3(Input.GetAxis("Mouse Y") * -rotSpd, Input.GetAxis("Mouse X") * rotSpd, 0));
             // targetRot = Quaternion.Euler(camRot);
-            fncGetTgtRot(transform.eulerAngles + (new Vector3(Input.GetAxis("Mouse Y") * -rotSpd, Input.GetAxis("Mouse X") * rotSpd, 0)));
-        }
     }
     void fncGetTgtRot(Vector3 getTgt)
     {
-        camRot = getTgt;
+        float lowerPitch = 360 - pitchLimit;
+        camRot = getTgt+transform.eulerAngles;
+        camRot.z = 0;
+        //  camRot.x = (camRot.x > pitchLimit) ? pitchLimit : ((camRot.x < -pitchLimit) ? -pitchLimit : pitchLimit); 
+        // camRot.x = Mathf.Clamp(camRot.x, -pitchLimit, pitchLimit);
+        // camRot.x = (camRot.x <pitchLimit)&&!(camRot.x>lowerPitch) ? (camRot.x > pitchLimit ? pitchLimit : camRot.x) : (camRot.x <0 ? (360 - pitchLimit) : camRot.x);
+        camRot.x = (camRot.x > 180) ? (camRot.x < lowerPitch ? lowerPitch : camRot.x) : (camRot.x > pitchLimit ? pitchLimit : camRot.x);
+        float qLim = pitchLimit / 360;
         targetRot = Quaternion.Euler(camRot);
-        avatarRot = Quaternion.Euler(new Vector3(0, getTgt.y, 0));
+        // targetRot.x = 
+        // avatarRot = Quaternion.Euler(new Vector3(0, getTgt.y, 0));
     }
+
+    Vector3 chaseVel;
     void FixedUpdate()
     {
+        if(camRig.position!=locPlyr.position)
+            transform.position = Vector3.SmoothDamp(transform.position, locPlyr.position, ref chaseVel, .5f, 20, Time.fixedDeltaTime);
         if (camRig.rotation != targetRot)
-            camRig.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, 15);
+            camRig.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, 1);
     }
 }
--- MJ77/Assets/Script/PlayerMove.cs
+++ MJ77/Assets/Script/PlayerMove.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public Rigidbody thisRB;
+    public CamScript CamSys;
     public float CurMove, MvSpd, SprintSpd, RotSpd, Accelerate;
     public float JmpPwr, JmpCntr, JmpTime;
 
     public bool isMove, isJump, isClimb;
 
-    Vector3 getInputMove { get { return new Vector3(Input.GetAxis("Horizontal"),0, (Input.GetAxis("Vertical"))); } }
-    [SerializeField] Vector3 inputMove, getRot, getMove;
+    Vector3 getInputMove { get { return new Vector3(Input.GetAxis("Horizontal"), 0, (Input.GetAxis("Vertical"))); } }
+    Vector3 inputMove, getRot, getMove;
     Quaternion targetRot;
     // Start is called before the first frame update
     // Start is called before the first frame update
@@ -26,7 +27,7 @@ public class PlayerMove : MonoBehaviour
     {
         inputMove = getInputMove;
         isMove = inputMove.sqrMagnitude >= .1f;
-        isJump = Input.GetAxis("Jump") >= .1f;
+        isJump = Input.GetKey(KeyCode.Space);//Input.GetAxis("Jump") >= .1f;
         CurMove = Input.GetKey(KeyCode.LeftShift) ? SprintSpd : MvSpd;
         // if (isMove)
         // {
@@ -35,21 +36,22 @@ public class PlayerMove : MonoBehaviour
         // else
         // { numAccel -= Time.deltaTime; }
 
-        // if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
-        // {
-        //     // getRot.x = thisRB.rotation.x + (Input.GetAxis("Mouse Y") * -RotSpd);
-        //     getRot.y = thisRB.rotation.y + (Input.GetAxis("Mouse X") * RotSpd);
-        //     targetRot = Quaternion.Euler(thisRB.rotation.eulerAngles + getRot);
-        //     rotTime = 0;
-        // }
-        
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            // getRot.x = thisRB.rotation.x + (Input.GetAxis("Mouse Y") * -RotSpd);
+            getRot = Vector3.zero;
+            getRot.y = CamSys.avatarRot(transform.rotation).eulerAngles.y;//CamSys.camRig.eulerAngles.y;//thisRB.rotation.y + (Input.GetAxis("Mouse X") * CamSys.rotSpd);
+            targetRot = CamSys.avatarRot(transform.rotation);//Quaternion.Euler();//thisRB.rotation.eulerAngles + getRot);
+        }
+
+
     }
-    float rotTime;
     void FixedUpdate()
     {
         if (isMove)
         {
-            getMove = transform.TransformDirection(inputMove * Mathf.Lerp(0, CurMove, Accelerate * Accelerate));
+            getMove = CamSys.camRig.TransformDirection(inputMove * Mathf.Lerp(0, CurMove, Accelerate * Accelerate));
+            thisRB.MovePosition(transform.position + (getMove * CurMove*Time.fixedDeltaTime));
             // thisRB.velocity = (inputMove * CurMove);// * Time.fixedDeltaTime;
             // if (thisRB.velocity.sqrMagnitude > getMove.sqrMagnitude)
             // {
@@ -60,14 +62,20 @@ public class PlayerMove : MonoBehaviour
             //     getMove.z = (thisRB.velocity.z - getMove.z);//(getMove.z > thisRB.velocity.z)?0 : getMove.z;
             // }
             // getMove.y = thisRB.velocity.y;
-            print($"Movement : {getMove} by {inputMove}");
-            thisRB.AddForce(getMove, ForceMode.Impulse);
-            if (thisRB.velocity.magnitude > MvSpd)
-                thisRB.velocity = new Vector3(thisRB.velocity.normalized.x * CurMove,
-                thisRB.velocity.y, thisRB.velocity.normalized.z * CurMove);
-            if (Accelerate <= 1)
-                Accelerate += Time.deltaTime;
-            else Accelerate = 1;
+            // print($"Movement : {getMove} by {inputMove}");
+            // if (thisRB.velocity.magnitude > MvSpd)
+            // {
+            //     getMove.x = Mathf.Abs(thisRB.velocity.x) > MvSpd ? (thisRB.velocity.x - getMove.x) : getMove.x;
+            //     getMove.y = Mathf.Abs(thisRB.velocity.y) > MvSpd ? (thisRB.velocity.y - getMove.y) : getMove.y;
+            // }
+            // thisRB.AddForce(getMove, ForceMode.Impulse);
+            // if (thisRB.velocity.magnitude > MvSpd)
+            //     thisRB.velocity = new Vector3(thisRB.velocity.normalized.x * CurMove,
+            //     thisRB.velocity.y, thisRB.velocity.normalized.z * CurMove);
+            // if (Accelerate <= 1)
+            //     Accelerate += Time.deltaTime;
+            // else Accelerate = 1;
+            Accelerate = Accelerate > 1 ? 1 : Accelerate + Time.fixedDeltaTime;
         }
         else
         {
@@ -96,9 +104,18 @@ public class PlayerMove : MonoBehaviour
         // if(isMove&& (isJump&&JmpCntr<JmpTime))
         //     thisRB.AddForce(getMove, ForceMode.VelocityChange);
 
-        if (thisRB.rotation != targetRot)
-        {
-            thisRB.MoveRotation(targetRot);
-        }
+        // if (thisRB.rotation != targetRot)
+        // {
+        //     thisRB.MoveRotation(targetRot);
+        // }
+        // if(transform.eulerAngles.y!=CamSys.targetRot.y)
+        //     transform.Rotate(CamSys.avatarRot(transform.rotation).eulerAngles);
+        // thisRB.MoveRotation(CamSys.avatarRot(transform.rotation));//Quaternion.Slerp(thisRB.rotation, CamSys.avatarRot, CamSys.rotSpd);
+        // if (transform.eulerAngles.y != getRot.y)
+        //     transform.eulerAngles = Vector3.SmoothDamp(transform.eulerAngles, getRot, ref deltaRot, .3f, CamSys.rotSpd);
+        // targetRot;//Quaternion.Lerp(thisRB.rotation, targetRot, Time.fixedDeltaTime);
+        if(transform.rotation!=targetRot)
+            transform.rotation = targetRot;
     }
+    Vector3 deltaRot;
 }
